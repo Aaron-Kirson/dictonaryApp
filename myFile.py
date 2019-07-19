bdd10b546af2a1f645a35d466970de283c2ff3b8
Aaron Kirson <aaronkirson@outlook.com>
2019-07-20 00:30:29 +0100

    fixed bug on did u mean error

--- myFile.py
+++ myFile.py
@@ -11,33 +11,41 @@ def translate(w):
 
     elif len(get_close_matches(w,data.keys())) > 0:
         yn =  input("did you mean %s instead? type y if yes or n if no: " %  get_close_matches(w, data.keys())[0])
+    else:
+            return "We didn't understand your entry."
 
-    if yn == "y":
-        return get_close_matches(w, data.keys())[0]
+    if yn == "y" or yn == "yes":
+        return data[get_close_matches(w, data.keys())[0]]
 
     else:
         return "no word found. please double check it"
 
 
+def start_app(word):
+    output = translate(word)
 
-word = input("please enter word: ")
+    if type(output) == list:
+        for item in output:
+            print(item)
+    else:
+        print(output)
 
-output = translate(word)
 
-if type(output) == list:
-    for item in output:
-        print(item)
-else:
-    print(output)
+
+word = input("please enter a word to find out the meaning: ")
+start_app(word)
+
+
 
 
 while True:
-    restart = input("Would you like to restart this program? type y if yes or n if no ")
+    restart = input("Would you like to restart this program? type y if yes or n if no: ")
 
-    if restart == "yes" or restart == "y":
-        word = input("please enter word: ")
-        output = translate(word)
 
-    if restart == "n" or restart == "no":
+    if restart.lower() == "yes" or restart.lower() == "y":
+        word = input("please enter a word to find out the meaning: ")
+        start_app(word)
+
+    elif restart == "n" or restart == "no":
              print ("program terminating. Goodbye.")
              break
